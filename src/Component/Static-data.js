fd3df9f0abcfcafe143fadc4d1afe48ee1ff1fc2
Carly Morris <thabai@hotmail.com>
2021-06-29 20:33:18 +0100

    functional

---
+++ src/Component/Static-data.js
@@ -0,0 +1,16 @@
+let items = [
+    {
+        id: 0,
+        name: 'Apple Ipad',
+        descriptions: 'The newest Ipad',
+        price: 399.99
+    },
+    {
+        id: 1,
+        name: 'Apple Ipad Pro',
+        descriptions: 'The newest professional Ipad',
+        price: 499.99
+    },
+];
+
+export {items};
